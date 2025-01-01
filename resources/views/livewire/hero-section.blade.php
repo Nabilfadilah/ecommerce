0231aa0d2d3e84595ec0cfd391dd4d60e8d61ce3
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-01 20:22:44 +0700

    Membuat view livewire hero section

---
+++ resources/views/livewire/hero-section.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <section class="bg-gray-50">
+        <div class="mx-auto max-w-screen-xl px-4 py-10 lg:flex lg:items-center">
+            <div class="mx-auto max-w-xl text-center">
+                <h1 class="text-3xl font-extrabold sm:text-5xl">
+                    Online Marketplace.
+                    <strong class="font-extrabold text-teal-700 sm:block text-3xl"> Discover Quality Products Online Now.
+                    </strong>
+                </h1>
+
+                <p class="mt-4 sm:text-xl/relaxed">
+                    Browse our collection of high-quality products and enjoy seamless online shopping.
+                </p>
+
+                <div class="mt-8 flex flex-wrap justify-center gap-4">
+                    <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
+                        href="#">
+                        Get Started
+                    </a>
+
+                    <a class="block w-full rounded px-12 py-3 text-sm font-medium text-teal-600 shadow hover:text-teal-700 focus:outline-none focus:ring active:text-teal-500 sm:w-auto"
+                        href="#">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
