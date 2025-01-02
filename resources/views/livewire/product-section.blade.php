36ae9d0eb38c8c17457038b6e6c704609fc95dff
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-02 10:06:37 +0700

    Membuat view livewire nama section setiap product

---
+++ resources/views/livewire/product-section.blade.php
@@ -0,0 +1,17 @@
+<div class="px-10 md:px-20 ml-30 py-3">
+    {{-- brand new --}}
+    <h2 class="font-medium text-[20px] my-3">Brand New</h2>
+    <livewire:product-listing />
+
+    {{-- smartphones & laptop --}}
+    <h2 class="font-medium text-[20px] my-3">Smartphones & laptops</h2>
+    <livewire:product-listing />
+
+    {{-- outfits --}}
+    <h2 class="font-medium text-[20px] my-3">Outfits</h2>
+    <livewire:product-listing />
+
+    {{-- digital products --}}
+    <h2 class="font-medium text-[20px] my-3">Digital Products</h2>
+    <livewire:product-listing />
+</div>
