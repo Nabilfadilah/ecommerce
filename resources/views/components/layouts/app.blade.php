ae9775692c1383b68772f6192022325d37f55c47
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-02 11:16:55 +0700

    Membuat view component layout liveware

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Detail Product' }}</title>
+    @vite('resources/css/app.css')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <livewire:header />
+    {{ $slot }}
+    <livewire:footer />
+</body>
+
+</html>
