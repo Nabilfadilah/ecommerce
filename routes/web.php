94d6ccb20faf60143d9fe13af7872fc535269f67
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-02 11:15:28 +0700

    Menambah route get product details

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\ProductDetails;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// detail product
+Route::get('/product/details', ProductDetails::class);
