c5cce082ab4302ee497f8004505f33acb56ab024
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-02 17:12:36 +0700

    Memberikan authentikasi untuk button /offer

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -13,10 +13,18 @@
                 </p>
 
                 <div class="mt-8 flex flex-wrap justify-center gap-4">
-                    <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
-                        href="#">
-                        Get Started
-                    </a>
+
+                    @if (auth()->check())
+                        <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
+                            href="/offer">
+                            Redeem your offer Now!
+                        </a>
+                    @else
+                        <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
+                            href="/auth/login">
+                            Get Started
+                        </a>
+                    @endif
 
                     <a class="block w-full rounded px-12 py-3 text-sm font-medium text-teal-600 shadow hover:text-teal-700 focus:outline-none focus:ring active:text-teal-500 sm:w-auto"
                         href="#">
