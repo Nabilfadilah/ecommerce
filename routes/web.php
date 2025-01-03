cc92e385c78156650083aeccd17585d978cf02bf
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-03 20:27:03 +0700

    Menambah route group untuk role admin

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/', function () {
 Route::get('/product/details', ProductDetails::class);
 
 // admin
-Route::get('/admin/dashboard', AdminDashboard::class)->name('dashboard')->middleware('admin');
-Route::get('/admin/products', ManageProduct::class)->name('products')->middleware('admin');
-Route::get('/admin/orders', ManageOrders::class)->name('orders')->middleware('admin');
-Route::get('/admin/products/add', AddProductForm::class)->middleware('admin');
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin/dashboard', AdminDashboard::class)->name('dashboard');
+    Route::get('/admin/products', ManageProduct::class)->name('products');
+    Route::get('/admin/orders', ManageOrders::class)->name('orders');
+    Route::get('/admin/products/add', AddProductForm::class);
+});
