d95b020146703aa35483e0d316db4451844e45cd
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-03 22:35:59 +0700

    Memberikan current url dan explode url untuk manage orders

--- app/Livewire/ManageOrders.php
+++ app/Livewire/ManageOrders.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class ManageOrders extends Component
 {
+    public $currentUrl;
+
     public function render()
     {
+        $current_url = url()->current();
+        $explode_url = explode('/', $current_url);
+
+        $this->currentUrl = $explode_url[3] . ' ' . $explode_url[4];
+
         return view('livewire.manage-orders')->layout('admin-layout');
     }
 }
