040b8de921e14ed791a57fcf198285dda8c31807
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-03 20:22:03 +0700

    Menambah route get untuk admin products dan orders

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Livewire\AddProductForm;
 use App\Livewire\AdminDashboard;
+use App\Livewire\ManageOrders;
+use App\Livewire\ManageProduct;
 use App\Livewire\ProductDetails;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +13,8 @@ Route::get('/', function () {
 
 // detail product
 Route::get('/product/details', ProductDetails::class);
-Route::get('/admin/dashboard', AdminDashboard::class)->middleware('admin');
+
+// admin
+Route::get('/admin/dashboard', AdminDashboard::class)->name('dashboard')->middleware('admin');
+Route::get('/admin/products', ManageProduct::class)->name('products')->middleware('admin');
+Route::get('/admin/orders', ManageOrders::class)->name('orders')->middleware('admin');
