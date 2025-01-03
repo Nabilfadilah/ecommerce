6f4cc52dfef6d548484b5b8e645ac5ec095fa043
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-03 20:19:31 +0700

    Memberikan current url dan explode url untuk nama breadcrumb

--- app/Livewire/AdminDashboard.php
+++ app/Livewire/AdminDashboard.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class AdminDashboard extends Component
 {
+    public $currentUrl;
+
     public function render()
     {
+        $current_url = url()->current();
+        $explode_url = explode('/', $current_url);
+
+        $this->currentUrl = $explode_url[3] . ' ' . $explode_url[4];
+
         return view('livewire.admin-dashboard')
             ->layout('admin-layout'); // Pastikan path layout sesuai
     }
