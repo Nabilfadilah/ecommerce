2ba943a4080785b6a354611f525901ecd7456f06
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-04 21:27:28 +0700

    Menambah function mount untuk get product item cart list

--- app/Livewire/ItemCard.php
+++ app/Livewire/ItemCard.php
@@ -6,6 +6,39 @@ use Livewire\Component;
 
 class ItemCard extends Component
 {
+    public $product;
+
+    public function mount($product_details)
+    {
+        $this->product = $product_details;
+    }
+
+    public function placeholder()
+    {
+        return view('livewire.skeleton.item-skeleton');
+    }
+
+    // add item ke cart 
+    // public function addToCart($productId)
+    // {
+    //     $cartItem = ShoppingCart::where('user_id', Auth::id())
+    //         ->where('product_id', $productId)
+    //         ->first();
+
+    //     if ($cartItem) {
+    //         $cartItem->quantity += 1; // increment its quantity
+    //         $cartItem->save();
+    //     } else {
+    //         ShoppingCart::create([
+    //             'user_id' => Auth::id(),
+    //             'product_id' => $productId,
+    //             'quantity' => 1,
+    //         ]);
+    //     }
+    //     //dispatch
+    //     $this->dispatch('cartUpdated');
+    // }
+
     public function render()
     {
         return view('livewire.item-card');
