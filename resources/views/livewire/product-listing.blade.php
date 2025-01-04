e2efb4ec23d03f681af41f8a1cf0174f33e83790
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-04 21:24:12 +0700

    Merubah product listing dengan kondisi mapping product dari database

--- resources/views/livewire/product-listing.blade.php
+++ resources/views/livewire/product-listing.blade.php
@@ -1,7 +1,26 @@
 <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
     {{-- import view item card --}}
-    <livewire:item-card />
-    <livewire:item-card />
-    <livewire:item-card />
-    <livewire:item-card />
+    @if (count($products) > 0)
+        @foreach ($products as $product)
+            <livewire:item-card lazy :product_details="$product" wire:key="{{ $product->id }}" />
+        @endforeach
+    @else
+        <h2 class="text-2xl text-gray-300">No Product Available</h2>
+    @endif
 </div>
+
+{{-- <div>
+    @if (Request::is('all/products') || Request::is('livewire/update'))
+        @include('components.search-component')
+    @endif
+    <div class="grid grid-cols-2 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-3">
+        @if (count($products) > 0)
+            @foreach ($products as $product)
+                <livewire:item-card lazy :product_details="$product" wire:key="{{$product->id}}"/>
+            @endforeach
+        @else
+            <h2 class="text-2xl text-gray-300">No Product Available</h2>
+        @endif
+        
+    </div>
+</div> --}}
