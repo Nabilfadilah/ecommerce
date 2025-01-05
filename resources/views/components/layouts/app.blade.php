0379337274cc6c3a688fa1cd5e8168bee9a2d9d6
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-05 11:18:30 +0700

    Merubah text title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $title ?? 'Detail Product' }}</title>
+    <title>{{ $title ?? 'Page Title' }}</title>
     @vite('resources/css/app.css')
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     @vite('resources/js/app.js')
