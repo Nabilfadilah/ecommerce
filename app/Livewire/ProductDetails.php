1efa578ffc48cf67629e7884b3f01eb69d2c6fed
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-05 11:21:26 +0700

    Menjalankan fungsi add to cart pada product detail

--- app/Livewire/ProductDetails.php
+++ app/Livewire/ProductDetails.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 use App\Models\Product;
+use App\Models\ShoppingCart;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ProductDetails extends Component
@@ -14,25 +16,25 @@ class ProductDetails extends Component
     }
 
     // add item to cart
-    // public function addToCart($productId)
-    // {
-    //     $cartItem = ShoppingCart::where('user_id', Auth::id())
-    //         ->where('product_id', $productId)
-    //         ->first();
+    public function addToCart($productId)
+    {
+        $cartItem = ShoppingCart::where('user_id', Auth::id())
+            ->where('product_id', $productId)
+            ->first();
 
-    //     if ($cartItem) {
-    //         $cartItem->quantity += 1; // increment its quantity
-    //         $cartItem->save();
-    //     } else {
-    //         ShoppingCart::create([
-    //             'user_id' => Auth::id(),
-    //             'product_id' => $productId,
-    //             'quantity' => 1,
-    //         ]);
-    //     }
-    //     //dispatch
-    //     $this->dispatch('cartUpdated');
-    // }
+        if ($cartItem) {
+            $cartItem->quantity += 1; // menambah quantitas
+            $cartItem->save();
+        } else {
+            ShoppingCart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $productId,
+                'quantity' => 1,
+            ]);
+        }
+        //dispatch
+        $this->dispatch('cartUpdated');
+    }
 
     public function render()
     {
