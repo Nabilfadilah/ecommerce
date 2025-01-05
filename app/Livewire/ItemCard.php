5f8530b8e4dc0b382dafd9d59fdd5b6b32e0641c
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-05 11:17:26 +0700

    Menjalankan fungsi addtocart untuk product berdasarkan id daru userId, productId dan quantity

--- app/Livewire/ItemCard.php
+++ app/Livewire/ItemCard.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\ShoppingCart;
+use Illuminate\Support\Facades\Auth;
 
 class ItemCard extends Component
 {
@@ -19,25 +21,25 @@ class ItemCard extends Component
     }
 
     // add item ke cart 
-    // public function addToCart($productId)
-    // {
-    //     $cartItem = ShoppingCart::where('user_id', Auth::id())
-    //         ->where('product_id', $productId)
-    //         ->first();
+    public function addToCart($productId)
+    {
+        $cartItem = ShoppingCart::where('user_id', Auth::id())
+            ->where('product_id', $productId)
+            ->first();
 
-    //     if ($cartItem) {
-    //         $cartItem->quantity += 1; // increment its quantity
-    //         $cartItem->save();
-    //     } else {
-    //         ShoppingCart::create([
-    //             'user_id' => Auth::id(),
-    //             'product_id' => $productId,
-    //             'quantity' => 1,
-    //         ]);
-    //     }
-    //     //dispatch
-    //     $this->dispatch('cartUpdated');
-    // }
+        if ($cartItem) {
+            $cartItem->quantity += 1; // menambah quantitas
+            $cartItem->save();
+        } else {
+            ShoppingCart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $productId,
+                'quantity' => 1,
+            ]);
+        }
+        //dispatch
+        $this->dispatch('cartUpdated');
+    }
 
     public function render()
     {
