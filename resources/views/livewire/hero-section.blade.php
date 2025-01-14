6e76c8b7e28cc45e1e8e32870028a83b3f182580
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-15 06:59:34 +0700

    Merubah text dan background color hero section

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -1,34 +1,35 @@
 <div>
-    <section class="bg-gray-50">
+    <section class="bg-gray-200">
         <div class="mx-auto max-w-screen-xl px-4 py-10 lg:flex lg:items-center">
             <div class="mx-auto max-w-xl text-center">
                 <h1 class="text-3xl font-extrabold sm:text-5xl">
                     Online Marketplace.
-                    <strong class="font-extrabold text-teal-700 sm:block text-3xl"> Discover Quality Products Online Now.
+                    <strong class="font-extrabold text-teal-700 sm:block text-3xl"> Temukan Produk Berkualitas Secara
+                        Online Sekarang.
                     </strong>
                 </h1>
 
                 <p class="mt-4 sm:text-xl/relaxed">
-                    Browse our collection of high-quality products and enjoy seamless online shopping.
+                    Telusuri koleksi produk kami dan nikmati belanja online yang lancar.
                 </p>
 
                 <div class="mt-8 flex flex-wrap justify-center gap-4">
 
                     @if (auth()->check())
-                        <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
+                        <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow-xl shadow-gray-500 hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
                             href="/offer">
-                            Redeem your offer Now!
+                            Tukarkan penawaran Anda Sekarang!
                         </a>
                     @else
-                        <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
+                        <a class="block w-full rounded bg-teal-600 px-12 py-3 text-sm font-medium text-white shadow-xl shadow-gray-500 hover:bg-teal-700 focus:outline-none focus:ring active:bg-teal-500 sm:w-auto"
                             href="/auth/login">
-                            Get Started
+                            Mulai sekarang
                         </a>
                     @endif
 
-                    <a class="block w-full rounded px-12 py-3 text-sm font-medium text-teal-600 shadow hover:text-teal-700 focus:outline-none focus:ring active:text-teal-500 sm:w-auto"
+                    <a class="block w-full rounded px-12 py-3 text-sm font-medium bg-gray-200 text-teal-600 shadow-xl shadow-gray-500 hover:text-teal-700 focus:outline-none focus:ring active:text-teal-500 sm:w-auto"
                         href="#">
-                        Learn More
+                        Pelajari lebih lanjut
                     </a>
                 </div>
             </div>
