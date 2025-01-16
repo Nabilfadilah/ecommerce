cc55ccb55fa6bcec03a6c924b4be4a69c0069585
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-16 10:03:01 +0700

    Merubah text dan category_id list product

--- resources/views/livewire/product-section.blade.php
+++ resources/views/livewire/product-section.blade.php
@@ -1,6 +1,6 @@
 <div class="px-10 md:px-20 ml-30 py-3">
     {{-- brand new --}}
-    <h2 class="font-medium text-[20px] my-3">Brand New</h2>
+    <h2 class="font-medium text-[20px] my-3">Product Baru</h2>
     <livewire:product-listing :category_id="0" :current_product_id="0" />
     {{-- @include('components.navigation.view-all', [
         'Category' => 'Brand New',
@@ -8,8 +8,8 @@
     <livewire:product-listing :category_id="0" :current_product_id="0" /> --}}
 
     {{-- smartphones & laptop --}}
-    <h2 class="font-medium text-[20px] my-3">Digital Products</h2>
-    <livewire:product-listing category_id="4" :current_product_id="0" />
+    <h2 class="font-medium text-[20px] my-3">Perangkat Output</h2>
+    <livewire:product-listing category_id="5" :current_product_id="0" />
     {{-- @include('components.navigation.view-all', [
         'Category' => 'Digital Products',
     ])
@@ -17,8 +17,8 @@
 
 
     {{-- outfits --}}
-    <h2 class="font-medium text-[20px] my-3">Fashion and Apparel</h2>
-    <livewire:product-listing :category_id="1" :current_product_id="0" />
+    <h2 class="font-medium text-[20px] my-3">Sepatu Running</h2>
+    <livewire:product-listing :category_id="7" :current_product_id="0" />
     {{-- @include('components.navigation.view-all', [
         'Category' => 'Fashion and Apparel',
     ])
